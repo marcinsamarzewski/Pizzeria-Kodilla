0a4939c409c9cad3d303c340358adcfc0fc51ee4
Marcin Samarzewski <m.samarzewski1990@gmail.com>
2021-08-02 21:35:10 +0200

    add js code to add menu product section

--- src/js/script.js
+++ src/js/script.js
@@ -298,6 +298,7 @@
       thisCart.dom = {};
       thisCart.dom.wrapper = element;
       thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+      thisCart.dom.productList = element.querySelector(select.cart.productList);
     }
     initActions(){
       const thisCart = this;
@@ -306,8 +307,11 @@
       });
     }
     add(menuProduct){
-      //const thisCart = this;
+      const thisCart = this;
       console.log('adding product', menuProduct);
+      const generatedHTML = templates.cartProduct(menuProduct);
+      const generatedDOM = utils.createDOMFromHTML(generatedHTML);
+      thisCart.dom.productList.appendChild(generatedDOM);
     }
   }
   const app = {

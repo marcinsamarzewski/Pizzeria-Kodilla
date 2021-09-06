74a3177e4b716cb0901ca321347aaf340194fbbf
Marcin Samarzewski <m.samarzewski1990@gmail.com>
2021-09-06 22:46:07 +0200

    render booking

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import {settings,select,classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 
 const app = {
@@ -70,6 +71,11 @@ const app = {
         thisApp.initMenu();
       });
   },
+  initBooking: function(){
+    const thisApp = this;
+    const bookingContainer = document.querySelector(select.containerOf.booking);
+    thisApp.booking = new Booking(bookingContainer);
+  },
   init: function(){
     const thisApp = this;
     // console.log('*** App starting ***');
@@ -80,6 +86,7 @@ const app = {
     thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
+    thisApp.initBooking();
   },
 };
 app.init();
